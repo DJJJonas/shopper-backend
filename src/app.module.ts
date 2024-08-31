3c915f10c9b4ab1159367f45fbcdcc0b11180c7c
DJJJonas <jonasmelo@duck.com>
2024-08-31 20:16:02 -0300

    chore: remove unnecessary metadata

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { CustomerModule } from './domains/customer/customer.module';
-import { CustomerService } from './domains/customer/service/customer.service';
-import { CustomerController } from './domains/customer/web/customer.controller';
-import { MeasureModule } from './domains/measure/measure.module';
-import { MeasureService } from './domains/measure/service/measure.service';
-import { MeasureController } from './domains/measure/web/measure.controller';
-import { GeminiModule } from './providers/gemini/gemini.module';
 import configModuleOptions from './config/config-module-options.config';
 import typeormModuleOptions from './config/typeorm-module-options.config';
+import { CustomerModule } from './domains/customer/customer.module';
+import { MeasureModule } from './domains/measure/measure.module';
+import { GeminiModule } from './providers/gemini/gemini.module';
 
 @Module({
   imports: [
@@ -20,7 +16,5 @@ import typeormModuleOptions from './config/typeorm-module-options.config';
     MeasureModule,
     CustomerModule,
   ],
-  controllers: [MeasureController, CustomerController],
-  providers: [MeasureService, CustomerService],
 })
 export class AppModule {}
